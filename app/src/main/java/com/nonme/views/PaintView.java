75467b504bccdf878d37175df7ba36626a75e583
nonme <xdock1good@gmail.com>
2019-06-06 01:48:59 +0300

    Drawing significally improved, however, out of memory error needs to be fixed when reading and drawing bitmaps.

--- app/src/main/java/com/nonme/views/PaintView.java
+++ app/src/main/java/com/nonme/views/PaintView.java
@@ -1,5 +1,6 @@
 package com.nonme.views;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -7,6 +8,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PointF;
+import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.support.annotation.ColorLong;
 import android.support.v4.content.ContextCompat;
@@ -15,7 +17,9 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
@@ -26,6 +30,7 @@ import com.nonme.actions.Action;
 import com.nonme.actions.Brush;
 import com.nonme.actions.Dropper;
 import com.nonme.actions.Image;
+import com.nonme.actions.ImageMoved;
 import com.nonme.actions.Shape;
 import com.nonme.actions.Text;
 import com.nonme.drawandpaint.ActionLab;
@@ -59,9 +64,20 @@ public class PaintView extends View
     private Action mCurrentTool;
     private ActionLab mActionLab;
     private Path mCirclePath;
+    private PointF mImageRelatives;
 
     private boolean mBitmapUpdated;
+    private boolean mIsMovingImage;
+    private boolean mTotalRepaintIsNeeded;
+    private boolean mLayoutCalledFirstTime;
+
     private Bitmap mBitmapImage;
+    private Bitmap mLastBitmap;
+    private Canvas mCanvas;
+    private ScaleGestureDetector mScaleDetector;
+    private float mScaleFactor = 1.f;
+    private int mDisplayWidth;
+    private int mDisplayHeight;
 
     public PaintView(Context context) {
         this(context, null);
@@ -101,8 +117,23 @@ public class PaintView extends View
 
         setFocusable(true);
         setFocusableInTouchMode(true);
-
         this.setDrawingCacheEnabled(true);
+        mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
+        mLayoutCalledFirstTime = true;
+    }
+    private class ScaleListener
+        extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            mScaleFactor *= detector.getScaleFactor();
+            mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 5.0f));
+            if(mCurrentToolType == Tools.IMAGE && mCurrentTool.getClass() == Image.class) {
+                ((ImageMoved) mCurrentTool).getImage().applyScale(mScaleFactor);
+                Log.i(TAG, "SCaling");
+                invalidate();
+            }
+            return true;
+        }
     }
     @Override
     public boolean dispatchKeyEvent(KeyEvent event) {
@@ -127,9 +158,21 @@ public class PaintView extends View
                 char charPressed = (char) event.getUnicodeChar();
                 ((Text) mCurrentTool).addChar(charPressed);
             }
-            invalidate();
+            redraw();
         }
         return true;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        if(mLayoutCalledFirstTime) {
+            mDisplayWidth = getWidth();
+            mDisplayHeight = getHeight();
+            mLastBitmap = Bitmap.createBitmap(mDisplayWidth, mDisplayHeight, Bitmap.Config.ARGB_8888);
+            mCanvas = new Canvas(mLastBitmap);
+            mLayoutCalledFirstTime = false;
+        }
     }
 
     @Override
@@ -139,23 +182,26 @@ public class PaintView extends View
             char charPressed = (char) event.getUnicodeChar();
             ((Text) mCurrentTool).addChar(charPressed);
             Log.i(TAG, "onKey " + charPressed);
-            invalidate();
+            redraw();
         }
         return true;
     }
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         PointF current = new PointF(event.getX(), event.getY());
+        mScaleDetector.onTouchEvent(event);
         switch(event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 actionDown(current);
+                draw();
                 break;
             case MotionEvent.ACTION_MOVE:
                 actionMove(current);
-                invalidate();
+                draw();
                 break;
             case MotionEvent.ACTION_UP:
                 actionUp(current);
+                updateDraw();
                 break;
         }
         return true;
@@ -189,6 +235,29 @@ public class PaintView extends View
                 mCurrentTool = new Shape(Shape.OVAL, current, mCurrentColor);
                 mActionLab.addAction(mCurrentTool);
                 break;
+            case Tools.IMAGE:
+                mCurrentToolType = Tools.IMAGE_MOVED;
+                mCurrentTool = new ImageMoved(((Image) mCurrentTool),
+                        ((Image) mCurrentTool).getOrigin().x,
+                        ((Image) mCurrentTool).getOrigin().y);
+            case Tools.IMAGE_MOVED:
+                Log.i(TAG, "Image chosen");
+                float leftM = ((ImageMoved) mCurrentTool).getImage().getOrigin().x;
+                float topM = ((ImageMoved) mCurrentTool).getImage().getOrigin().y;
+                float rightM = ((ImageMoved)mCurrentTool).getImage().getImage().getWidth()+leftM;
+                float bottomM = ((ImageMoved) mCurrentTool).getImage().getImage().getHeight()+topM;
+
+                boolean isInImageM = (current.x >= leftM && current.x <= rightM &&
+                        current.y >= topM && current.y <= bottomM);
+                if(isInImageM) {
+                    mCurrentTool = new ImageMoved(((ImageMoved) mCurrentTool).getImage(),
+                            leftM,
+                            topM);
+                    mActionLab.addAction(mCurrentTool);
+                    mImageRelatives = new PointF(current.x-leftM, current.y-topM);
+                    mIsMovingImage = true;
+                }
+                break;
         }
         mCirclePath.addCircle(current.x, current.y, 50, Path.Direction.CW);
     }
@@ -202,6 +271,12 @@ public class PaintView extends View
             case Tools.RECTANGLE:
             case Tools.OVAL:
                 ((Shape) mCurrentTool).setEnd(current);
+                break;
+            case Tools.IMAGE_MOVED:
+                if(mIsMovingImage)
+                    ((ImageMoved) mCurrentTool).setDestination(new PointF(
+                            current.x-mImageRelatives.x,
+                            current.y-mImageRelatives.y));
                 break;
         }
         mCirclePath.reset();
@@ -223,77 +298,115 @@ public class PaintView extends View
                 mCurrentTool = new Text(current.x, current.y,
                         R.font.lobster, mCurrentTextSize, mCurrentColor);
                 mActionLab.addAction(mCurrentTool);
+                break;
             case Tools.LINE:
             case Tools.RECTANGLE:
             case Tools.OVAL:
                 ((Shape) mCurrentTool).setEnd(current);
                 break;
+            case Tools.IMAGE_MOVED:
+                if(mIsMovingImage)
+                    ((ImageMoved) mCurrentTool).setDestination(new PointF(
+                            current.x-mImageRelatives.x,
+                            current.y-mImageRelatives.y));
+                mIsMovingImage = false;
         }
         mCirclePath.reset();
+    }
+    public void redraw() {
+        mTotalRepaintIsNeeded = true;
         invalidate();
+    }
+    public void draw() {
+        invalidate();
+    }
+    public void updateDraw() {
+        drawAction(mCanvas, mCurrentTool);
     }
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawPaint(mBackgroundPaint);
-        List<Action> drawList = mActionLab.getDrawables();
-        for(ListIterator<Action> l = drawList.listIterator(); l.hasNext(); ) {
-            if(l.nextIndex() > mActionLab.getCurrentAction())
-                break;
-            Action action = l.next();
-            if(action.getClass() == Brush.class) {
-                mPaint.setColor(((Brush) action).getColor());
-                mPaint.setStrokeWidth(((Brush) action).getBrushSize());
-                canvas.drawPath((Brush) action, mPaint);
+        if(mTotalRepaintIsNeeded) {
+            Log.i(TAG, "Total repaint");
+            mCanvas.drawPaint(mBackgroundPaint);
+            List<Action> drawList = mActionLab.getDrawables();
+            for (ListIterator<Action> l = drawList.listIterator(); l.hasNext(); ) {
+                if (l.nextIndex() > mActionLab.getCurrentAction())
+                    break;
+                Action action = l.next();
+                drawAction(mCanvas, action);
             }
-            else if(action.getClass() == Text.class) {
-                mTextPaint.setColor(((Text) action).getColor());
-                mTextPaint.setTextSize(((Text) action).getFontSize());
-                if(action == mCurrentTool)
-                    canvas.drawText(((Text) action).getText() + "|", ((Text) action).getOrigin().x,
-                            ((Text) action).getOrigin().y, mTextPaint);
-                else
-                    canvas.drawText(((Text) action).getText(), ((Text) action).getOrigin().x,
-                            ((Text) action).getOrigin().y, mTextPaint);
-            }
-            else if(action.getClass() == Image.class) {
-                Bitmap image = ((Image) action).getImage();
-                PointF position = ((Image) action).getOrigin();
-                canvas.drawBitmap(image, position.x, position.y, mPaint);
-            }
-            else if(action.getClass() == Shape.class) {
-                float left = ((Shape) action).getOrigin().x;
-                float right = ((Shape) action).getEnd().x;
-                float top = ((Shape) action).getOrigin().y;
-                float bottom = ((Shape) action).getEnd().y;
-                int color = ((Shape) action).getColor();
-                mPaint.setColor(color);
-                switch(((Shape)action).getType()) {
-                    case Shape.LINE:
-                        canvas.drawLine(left, top, right, bottom, mPaint);
-                        break;
-                    case Shape.RECT:
-                        if(left > right) {
-                            float temp = left; left = right; right = temp;
-                        }
-                        if(top > bottom) {
-                            float temp = top; top = bottom; bottom = temp;
-                        }
-                        canvas.drawRect(left, top, right, bottom, mPaint);
-                        break;
-                    case Shape.OVAL:
-                        if(left > right) {
-                            float temp = left; left = right; right = temp;
-                        }
-                        if(top > bottom) {
-                            float temp = top; top = bottom; bottom = temp;
-                        }
-                        canvas.drawOval(left, top, right, bottom, mPaint);
-                        break;
-                }
+            canvas.drawBitmap(mLastBitmap, 0, 0, mPaint);
+            mTotalRepaintIsNeeded = false;
+        }
+        else {
+            Action action = mActionLab.getLastDrawable();
+            if(action != null) {
+                canvas.drawBitmap(mLastBitmap, 0, 0, mPaint);
+                drawAction(canvas, action);
             }
         }
         canvas.drawPath(mCirclePath, mCirclePaint);
         mBitmapUpdated = true;
+    }
+    public void drawAction(Canvas canvas, Action action) {
+        if (action.getClass() == Brush.class) {
+            mPaint.setColor(((Brush) action).getColor());
+            mPaint.setStrokeWidth(((Brush) action).getBrushSize());
+            canvas.drawPath((Brush) action, mPaint);
+        } else if (action.getClass() == Text.class) {
+            mTextPaint.setColor(((Text) action).getColor());
+            mTextPaint.setTextSize(((Text) action).getFontSize());
+            if (action == mCurrentTool)
+                canvas.drawText(((Text) action).getText() + "|", ((Text) action).getOrigin().x,
+                        ((Text) action).getOrigin().y, mTextPaint);
+            else
+                canvas.drawText(((Text) action).getText(), ((Text) action).getOrigin().x,
+                        ((Text) action).getOrigin().y, mTextPaint);
+        } else if (action.getClass() == Image.class) {
+            Bitmap image = ((Image) action).getImage();
+            PointF position = ((Image) action).getOrigin();
+            Rect source = new Rect((int)position.x, (int) position.y, image.getWidth(), image.getHeight());
+            Rect dest = new Rect((int) position.x, (int) position.y, mDisplayWidth, mDisplayHeight);
+            canvas.drawBitmap(image, source, dest, mPaint);
+        } else if (action.getClass() == Shape.class) {
+            float left = ((Shape) action).getOrigin().x;
+            float right = ((Shape) action).getEnd().x;
+            float top = ((Shape) action).getOrigin().y;
+            float bottom = ((Shape) action).getEnd().y;
+            int color = ((Shape) action).getColor();
+            mPaint.setColor(color);
+            switch (((Shape) action).getType()) {
+                case Shape.LINE:
+                    canvas.drawLine(left, top, right, bottom, mPaint);
+                    break;
+                case Shape.RECT:
+                    if (left > right) {
+                        float temp = left;
+                        left = right;
+                        right = temp;
+                    }
+                    if (top > bottom) {
+                        float temp = top;
+                        top = bottom;
+                        bottom = temp;
+                    }
+                    canvas.drawRect(left, top, right, bottom, mPaint);
+                    break;
+                case Shape.OVAL:
+                    if (left > right) {
+                        float temp = left;
+                        left = right;
+                        right = temp;
+                    }
+                    if (top > bottom) {
+                        float temp = top;
+                        top = bottom;
+                        bottom = temp;
+                    }
+                    canvas.drawOval(left, top, right, bottom, mPaint);
+                    break;
+            }
+        }
     }
     public void setCurrentTool(int type) {
         mCurrentToolType = type;
@@ -316,12 +429,25 @@ public class PaintView extends View
     }
 
     public void openImage(Bitmap bitmap) {
-        mActionLab.addAction(new Image(bitmap));
-        invalidate();
+        mCurrentTool = new Image(bitmap);
+        mActionLab.addAction(mCurrentTool);
+        mIsMovingImage = false;
+        redraw();
     }
 
     public void clear() {
         mActionLab.clear();
-        invalidate();
+        redraw();
     }
+    public static void measureView(final View view, final Runnable runnable) {
+        final ViewTreeObserver.OnPreDrawListener preDrawListener = new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                view.getViewTreeObserver().removeOnPreDrawListener(this);
+                runnable.run();
+                return true;
+            }
+        };
+        view.getViewTreeObserver().addOnPreDrawListener(preDrawListener); }
+
 }
